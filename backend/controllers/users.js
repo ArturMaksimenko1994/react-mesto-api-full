8e7d4a0c22bd5c570f0843e3d7c60d464ed435d7
Arthur Maksimenko <arturmaksimenko784@gmail.com>
2022-10-16 00:31:06 +0300

    ver.54

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -149,11 +149,11 @@ const updateAvatar = (req, res, next) => {
 
 // информация о пользователе
 const getUserInfo = (req, res, next) => {
-  User.findById({ _id: req.user._id })
+  User.findById( req.user._id )
     .orFail(() => {
       throw new Error('NotFound');
     })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send({ user }))
     .catch((err) => {
       if (err.message === 'NotFound') {
         return next(new ErrorNotFound('Пользователь не найден'));
