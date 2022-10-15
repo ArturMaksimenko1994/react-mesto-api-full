730b24e3ea7b5cbce1bf34a35090db9e8d74e42c
Arthur Maksimenko <arturmaksimenko784@gmail.com>
2022-10-15 22:57:20 +0300

    ver.46

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -87,14 +87,12 @@ class Api {
   }
 }
 
-const token = localStorage.getItem('token');
+// const token = localStorage.getItem('token');
 
-const api = new Api({
+export const api = new Api({
   url: "https://api.artur.studen.nomoredomains.icu",
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
     "Content-Type": "application/json",
   },
 });
-
-export default api;
