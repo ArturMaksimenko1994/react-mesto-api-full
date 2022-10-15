e850802573692aa39d0b742d54743f582d6408c5
Arthur Maksimenko <arturmaksimenko784@gmail.com>
2022-10-15 23:36:38 +0300

    ver.50

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -29,7 +29,7 @@ const Login = ( {handleLogin} ) => {
             const { password, email} = data;
             auth.authorize(password, email).then((data) => {
                 if (data.token) {
-                    localStorage.setItem("token", res.token);
+                    localStorage.setItem("token", data.token);
                     handleLogin()
                     history.push('/')
                 }
