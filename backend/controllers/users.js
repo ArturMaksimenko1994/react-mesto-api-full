c11660512507a348504df01e7c48d907f097652d
Arthur Maksimenko <arturmaksimenko784@gmail.com>
2022-10-16 14:50:16 +0300

    ver.71

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -67,10 +67,10 @@ const login = (req, res, next) => {
     });
 };
 
-// возвращает всех пользователей
+// возвращает пол
 const getUsers = (req, res, next) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => res.send(users))
     .catch(next);
 };
 
